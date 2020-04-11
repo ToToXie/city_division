1de316e79cd7143ed7fffc61a7a431f3e07c1b42
WangDong <wdtodo@163.com>
2020-04-11 23:44:04 +0800

    feat: 使用上redis进行缓存

--- src/main/java/wd/city_division/config/ShiroConfig.java
+++ src/main/java/wd/city_division/config/ShiroConfig.java
@@ -1,15 +1,30 @@
 package wd.city_division.config;
 
+import at.pollux.thymeleaf.shiro.dialect.ShiroDialect;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.codec.Base64;
 import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.session.SessionListener;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.CookieRememberMeManager;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.servlet.SimpleCookie;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
+import org.crazycake.shiro.RedisCacheManager;
+import org.crazycake.shiro.RedisManager;
+import org.crazycake.shiro.RedisSessionDAO;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.Base64Utils;
 import wd.city_division.propertise.FebsProperties;
 
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.LinkedHashMap;
 
 /**
@@ -23,38 +38,38 @@ public class ShiroConfig {
 
     private final FebsProperties febsProperties;
 
-//    @Value("${spring.redis.host}")
-//    private String host;
-//    @Value("${spring.redis.port}")
-//    private int port;
-//    @Value("${spring.redis.password:}")
-//    private String password;
-//    @Value("${spring.redis.timeout}")
-//    private int timeout;
-//    @Value("${spring.redis.database:0}")
-//    private int database;
+    @Value("${spring.redis.host}")
+    private String host;
+    @Value("${spring.redis.port}")
+    private int port;
+    @Value("${spring.redis.password:}")
+    private String password;
+    @Value("${spring.redis.timeout}")
+    private int timeout;
+    @Value("${spring.redis.database:0}")
+    private int database;
 
     /**
      * shiro 中配置 redis 缓存
      *
      * @return RedisManager
      */
-//    private RedisManager redisManager() {
-//        RedisManager redisManager = new RedisManager();
-//        redisManager.setHost(host + ":" + port);
-//        if (StringUtils.isNotBlank(password)) {
-//            redisManager.setPassword(password);
-//        }
-//        redisManager.setTimeout(timeout);
-//        redisManager.setDatabase(database);
-//        return redisManager;
-//    }
+    private RedisManager redisManager() {
+        RedisManager redisManager = new RedisManager();
+        redisManager.setHost(host + ":" + port);
+        if (StringUtils.isNotBlank(password)) {
+            redisManager.setPassword(password);
+        }
+        redisManager.setTimeout(timeout);
+        redisManager.setDatabase(database);
+        return redisManager;
+    }
 
-//    private RedisCacheManager cacheManager() {
-//        RedisCacheManager redisCacheManager = new RedisCacheManager();
-//        redisCacheManager.setRedisManager(redisManager());
-//        return redisCacheManager;
-//    }
+    private RedisCacheManager cacheManager() {
+        RedisCacheManager redisCacheManager = new RedisCacheManager();
+        redisCacheManager.setRedisManager(redisManager());
+        return redisCacheManager;
+    }
 
     @Bean
     public ShiroFilterFactoryBean shiroFilterFactoryBean(SecurityManager securityManager) {
@@ -91,11 +106,11 @@ public class ShiroConfig {
         // 配置 SecurityManager，并注入 shiroRealm
         securityManager.setRealm(shiroRealm);
         // 配置 shiro session管理器
-//        securityManager.setSessionManager(sessionManager());
-//        // 配置 缓存管理类 cacheManager
-//        securityManager.setCacheManager(cacheManager());
-//        // 配置 rememberMeCookie
-//        securityManager.setRememberMeManager(rememberMeManager());
+        securityManager.setSessionManager(sessionManager());
+        // 配置 缓存管理类 cacheManager
+        securityManager.setCacheManager(cacheManager());
+        // 配置 rememberMeCookie
+        securityManager.setRememberMeManager(rememberMeManager());
         return securityManager;
     }
 
@@ -104,29 +119,29 @@ public class ShiroConfig {
      *
      * @return SimpleCookie
      */
-//    private SimpleCookie rememberMeCookie() {
-//        // 设置 cookie 名称，对应 login.html 页面的 <input type="checkbox" name="rememberMe"/>
-//        SimpleCookie cookie = new SimpleCookie("rememberMe");
-//        // 设置 cookie 的过期时间，单位为秒，这里为一天
-//        cookie.setMaxAge(febsProperties.getShiro().getCookieTimeout());
-//        return cookie;
-//    }
+    private SimpleCookie rememberMeCookie() {
+        // 设置 cookie 名称，对应 login.html 页面的 <input type="checkbox" name="rememberMe"/>
+        SimpleCookie cookie = new SimpleCookie("rememberMe");
+        // 设置 cookie 的过期时间，单位为秒，这里为一天
+        cookie.setMaxAge(febsProperties.getShiro().getCookieTimeout());
+        return cookie;
+    }
 
     /**
      * cookie管理对象
      *
      * @return CookieRememberMeManager
      */
-//    private CookieRememberMeManager rememberMeManager() {
-//        CookieRememberMeManager cookieRememberMeManager = new CookieRememberMeManager();
-//        cookieRememberMeManager.setCookie(rememberMeCookie());
-//        // rememberMe cookie 加密的密钥
-//        String encryptKey = "febs_shiro_key";
-//        byte[] encryptKeyBytes = encryptKey.getBytes(StandardCharsets.UTF_8);
-//        String rememberKey = Base64Utils.encodeToString(Arrays.copyOf(encryptKeyBytes, 16));
-//        cookieRememberMeManager.setCipherKey(Base64.decode(rememberKey));
-//        return cookieRememberMeManager;
-//    }
+    private CookieRememberMeManager rememberMeManager() {
+        CookieRememberMeManager cookieRememberMeManager = new CookieRememberMeManager();
+        cookieRememberMeManager.setCookie(rememberMeCookie());
+        // rememberMe cookie 加密的密钥
+        String encryptKey = "febs_shiro_key";
+        byte[] encryptKeyBytes = encryptKey.getBytes(StandardCharsets.UTF_8);
+        String rememberKey = Base64Utils.encodeToString(Arrays.copyOf(encryptKeyBytes, 16));
+        cookieRememberMeManager.setCipherKey(Base64.decode(rememberKey));
+        return cookieRememberMeManager;
+    }
 
     @Bean
     public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(SecurityManager securityManager) {
@@ -140,33 +155,33 @@ public class ShiroConfig {
      *
      * @return ShiroDialect shiro 方言对象
      */
-//    @Bean
-//    public ShiroDialect shiroDialect() {
-//        return new ShiroDialect();
-//    }
+    @Bean
+    public ShiroDialect shiroDialect() {
+        return new ShiroDialect();
+    }
 
-//    @Bean
-//    public RedisSessionDAO redisSessionDAO() {
-//        RedisSessionDAO redisSessionDAO = new RedisSessionDAO();
-//        redisSessionDAO.setRedisManager(redisManager());
-//        return redisSessionDAO;
-//    }
+    @Bean
+    public RedisSessionDAO redisSessionDAO() {
+        RedisSessionDAO redisSessionDAO = new RedisSessionDAO();
+        redisSessionDAO.setRedisManager(redisManager());
+        return redisSessionDAO;
+    }
 
     /**
      * session 管理对象
      *
      * @return DefaultWebSessionManager
      */
-//    @Bean
-//    public DefaultWebSessionManager sessionManager() {
-//        DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
-//        Collection<SessionListener> listeners = new ArrayList<>();
-//        listeners.add(new ShiroSessionListener());
-//        // 设置 session超时时间
-//        sessionManager.setGlobalSessionTimeout(febsProperties.getShiro().getSessionTimeout() * 1000L);
-//        sessionManager.setSessionListeners(listeners);
-//        sessionManager.setSessionDAO(redisSessionDAO());
-//        sessionManager.setSessionIdUrlRewritingEnabled(false);
-//        return sessionManager;
-//    }
+    @Bean
+    public DefaultWebSessionManager sessionManager() {
+        DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
+        Collection<SessionListener> listeners = new ArrayList<>();
+        listeners.add(new ShiroSessionListener());
+        // 设置 session超时时间
+        sessionManager.setGlobalSessionTimeout(febsProperties.getShiro().getSessionTimeout() * 1000L);
+        sessionManager.setSessionListeners(listeners);
+        sessionManager.setSessionDAO(redisSessionDAO());
+        sessionManager.setSessionIdUrlRewritingEnabled(false);
+        return sessionManager;
+    }
 }
